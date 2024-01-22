d0a4256d19f68b095fb8a54354ee1098510c783e
David Milton <8712ce@gmail.com>
2024-01-22 17:01:21 -0600

    created pseudocode for fourWayInt component.

---
+++ frontend/src/components/FourWayInt/index.js
@@ -0,0 +1,34 @@
+// DEPENDENCIES //
+import React from "react";
+import { Link } from "react-router-dom";
+
+// IMAGES //
+
+
+
+// STYLES //
+import "./4WayIntStyles.css";
+
+
+
+
+
+// Global functions
+
+
+
+
+
+
+
+
+
+export default function Chapters() {
+    return (
+        <div>
+
+           
+
+        </div>
+    )
+}
