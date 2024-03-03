6c54642c9068fd75fa9720af46ce7b59a07d206d
David Milton <8712ce@gmail.com>
2024-03-03 15:27:23 -0600

    added a few dependencies and routes to app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+// DEPENDENCIES //
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import React from "react";
+import axios from "axios";
 
+// PAGES //
+
+// COMPONENTS //
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// STYLES //
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+
+        <Route path="/header" element={<Header />} />
+
+        <Route path="/fourWaySignals" element={<FourWaySignals />} />
+
+        <Route path="/footer" element={<Footer />} />
+
+      </Routes>
       
 
 
