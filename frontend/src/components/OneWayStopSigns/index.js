230a5fdda14e2d6ee659b6c11ecc7d754e33fe2c
David Milton <8712ce@gmail.com>
2024-03-12 14:49:15 -0500

    added a red stop line for blue square to pass over.

--- frontend/src/components/OneWayStopSigns/index.js
+++ frontend/src/components/OneWayStopSigns/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './OneWayStopSigns.css';
 
 const OneWayStopSigns = () => {
-  return <div className="square"></div>;
+  return (
+    <div>
+      <div className="divider"></div>
+      <div className="square"></div>;
+    </div>
+  );
 };
+
 
 export default OneWayStopSigns;
