cf00f93a048756be21287ddf23f47a70b79df143
David Milton <8712ce@gmail.com>
2024-03-25 14:52:39 -0500

    created vehicle schema.

---
+++ backend/models/vehicles.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES //
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// CREATE A VEHICLE SCHEMA //
+const vehicleSchema = new Schema (
+    {
+        type: { type: String, required: true },
+        topSpeed: { type: Number, required: true },
+        damage: { type: Number, required: true },
+        image: { type: String, required: true }
+    }
+)
+
+// CREATE A VEHICLE MODEL USING THE VEHICLE SCHEMA //
+const Vehicle = mongoose.model('Vehicle', vehicleSchema)
+
+// EXPORT THE VEHICLE MODEL, WEILL BE ACCESSED IN INDEX.JS //
+module.exports = Vehicle
