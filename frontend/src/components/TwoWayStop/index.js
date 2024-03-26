516327eb81338232dc278c8f16cda964c59b73dc
David Milton <8712ce@gmail.com>
2024-03-26 15:54:40 -0500

    added east and west bound lanes to TwoWayStop component with some simple css.

--- frontend/src/components/TwoWayStop/index.js
+++ frontend/src/components/TwoWayStop/index.js
@@ -1,9 +1,25 @@
 import React from "react";
+import './TwoWayStop.css';
+
+
 
 const TwoWayStop = () => {
     return (
         <div className="two-way-stop-layout">
-            {/* LAYOUT FOR TWO-WAY INTERSECTION */}
+            {/* EASTBOUND LANE */}
+            <div className="lane eastbound">
+                {/* VEHICLES TRAVELING EAST */}
+                <div className="vehicle east" style={{ left: "0%" }}></div>
+            </div>
+
+            {/* WESTBOUND LANE */}
+            <div className="lane westbound">
+                {/* VEHICLES TRAVELING WEST */}
+                <div className="vehicle west" style={{ right: "0%" }}></div>
+            </div>
+
+            {/* CROSSWALK */}
+            <div className="crosswalk"></div>
         </div>
     );
 }
