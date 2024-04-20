0ed4df669de10ee5ef5d42d669573fb9f1ca31d6
David Milton <8712ce@gmail.com>
2024-04-20 15:50:30 -0500

    backend with vehicles controller and model working at local port

--- backend/controllers/vehicles.js
+++ backend/controllers/vehicles.js
@@ -4,18 +4,17 @@ const db = require('../models')
 const config = require('../config.js/config')
 
 // MODELS //
-const db = require('../models')
 const Vehicle = db.Vehicle
 
 // EXPRESS ROUTE TO ADD A VEHICLE //
-app.post('/add-car', (req, res) => {
+router.post('/add-car', (req, res) => {
     const newVehicle = { /* VEHICLE DATA */ }; // DEFINE VEHICLE DATA
     intersection.addVehicle(newVehicle)
     res.send('Vehicle Added');
 });
 
 // EXPRSES ROUTE TO REMOVE A CAR //
-app.delete('/remove-vehicle/:index', (req, res) => {
+router.delete('/remove-vehicle/:index', (req, res) => {
     const { index } = req.params;
     intersection.removeVehicle(index);
     res.send('Vehicle Removed');
