b48929b337a8e25f52cdf423e9027770e5f58bd4
David Milton <8712ce@gmail.com>
2024-04-29 19:08:05 -0500

    created a delete route for vehicle controller.

--- backend/controllers/vehicles.js
+++ backend/controllers/vehicles.js
@@ -1,23 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
-const config = require('../config.js/config')
+// const config = require('../config.js/config')
 
-// MODELS //
-const Vehicle = db.Vehicle
+// CREATE (POST) ROUTE:
 
-// EXPRESS ROUTE TO ADD A VEHICLE //
-router.post('/add-car', (req, res) => {
-    const newVehicle = { /* VEHICLE DATA */ }; // DEFINE VEHICLE DATA
-    intersection.addVehicle(newVehicle)
-    res.send('Vehicle Added');
-});
-
-// EXPRSES ROUTE TO REMOVE A CAR //
-router.delete('/remove-vehicle/:index', (req, res) => {
-    const { index } = req.params;
-    intersection.removeVehicle(index);
-    res.send('Vehicle Removed');
-});
+// DELETE ROUTE //
+router.delete('/:id', (req, res) => {
+    db.Vehicle.findByIdAndRemove(req.params.id, (err, vehicle) => {
+        // res.redirect('/')
+    })
+})
 
 module.exports = router;
