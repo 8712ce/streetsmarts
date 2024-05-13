c98b85e4a55aad0e24ec538b51ba18bd890622ae
David Milton <8712ce@gmail.com>
2024-05-13 16:51:59 -0500

    post route works. it successfully stores new vehicles with respective paths in db.  now the frontend automobile component needs to be updated to show the vehicles.

--- backend/controllers/vehicles.js
+++ backend/controllers/vehicles.js
@@ -6,25 +6,25 @@ const Vehicle = require('../models/vehicle');
 const Path = require('../models/path');
 
 
-// ROUTE TO FETCH A RANDOM VEHICLE WITH A RANDOM PATH //
-router.get('/random', async (req, res) => {
-    try {
-        // FETCH A RANDOM VEHICLE //
-        let randomVehicle = await Vehicle.aggregate([{ $sample: { size: 1 } }]);
-        console.log(randomVehicle)
-        // IF THE PATH OF THE RANDOM BEHICLE IS NULL, REPLACE IT WITH A RANDOM PATH //
-        if (randomVehicle[0].path === null) {
-            const randomPath = await Path.aggregate([{ $sample: { size: 1 } }]);
-            console.log(randomPath)
-            randomVehicle[0].path = randomPath[0].coordinates; // ASSUMING PATH IS STORED AS AN OBJECTID //
-        }
+// // ROUTE TO FETCH A RANDOM VEHICLE WITH A RANDOM PATH //
+// router.get('/random', async (req, res) => {
+//     try {
+//         // FETCH A RANDOM VEHICLE //
+//         let randomVehicle = await Vehicle.aggregate([{ $sample: { size: 1 } }]);
+//         console.log(randomVehicle)
+//         // IF THE PATH OF THE RANDOM BEHICLE IS NULL, REPLACE IT WITH A RANDOM PATH //
+//         if (randomVehicle[0].path === null) {
+//             const randomPath = await Path.aggregate([{ $sample: { size: 1 } }]);
+//             console.log(randomPath)
+//             randomVehicle[0].path = randomPath[0].coordinates; // ASSUMING PATH IS STORED AS AN OBJECTID //
+//         }
 
-        res.json(randomVehicle);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server Error' });
-    }
-});
+//         res.json(randomVehicle);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ message: 'Server Error' });
+//     }
+// });
 
 
 
@@ -58,6 +58,7 @@ router.get('/random', async (req, res) => {
 //         if (randomVehicle[0].path === null) {
 //             const randomPath = await Path.aggregate([{ $sample: { size: 1 } }]);
 //             randomVehicle[0].path = randomPath[0].coordinates; // ASSUMING PATH IS STORED AS AN OBJECTID //
+//             console.log(randomPath)
 //         }
 
 //         // CREATE THE RANDOM VEHICLE IN THE DATABASE //
@@ -70,6 +71,43 @@ router.get('/random', async (req, res) => {
 //         res.status(500).json({ message: 'Server Error' });
 //     }
 // });
+
+
+// ROUTE TO FETCH A RANDOM VEHICLE WITH A RANDOM PATH AND SAVE TO DATABASE //
+router.post('/random', async (req, res) => {
+    try {
+        // FETCH A RANDOM VEHICLE //
+        let randomVehicle = await Vehicle.aggregate([{ $sample: { size: 1 } }]);
+        console.log(randomVehicle)
+        
+        // IF THE PATH OF THE RANDOM VEHICLE IS NULL, REPLACE IT WITH A RANDOM PATH //
+        if (randomVehicle[0].path === null) {
+            const randomPath = await Path.aggregate([{ $sample: { size: 1 } }]);
+            randomVehicle[0].path = randomPath[0].coordinates; // ASSUMING PATH IS STORED AS AN OBJECTID //
+            console.log(randomPath)
+        }
+
+        // Create a new vehicle object without the _id field
+        const newVehicle = {
+            type: randomVehicle[0].type,
+            damage: randomVehicle[0].damage,
+            image: randomVehicle[0].image,
+            path: randomVehicle[0].path
+        };
+        console.log(newVehicle)
+
+        // CREATE THE RANDOM VEHICLE IN THE DATABASE //
+        const createdVehicle = await Vehicle.create(newVehicle);
+        console.log(createdVehicle)
+
+        // RETURN THE CREATED VEHICLE AS JSON RESPONSE //
+        res.json(createdVehicle);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
 
 
 
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -23,9 +23,19 @@ export async function createStudent(formData) {
 
 // Get random vehicle
 
+// export async function getRandomVehicle() {
+//     try {
+//         const {data} = await axios.get('http://localhost:8000/vehicles/random');
+//         return data;
+//     } catch (error) {
+//         console.error('Error fetching random vehicle:', error);
+//         return { error: 'Failed to fetch random vehicle' };
+//     }
+// }
+
 export async function getRandomVehicle() {
     try {
-        const {data} = await axios.get('http://localhost:8000/vehicles/random');
+        const {data} = await axios.post('http://localhost:8000/vehicles/random');
         return data;
     } catch (error) {
         console.error('Error fetching random vehicle:', error);
