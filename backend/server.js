ef906b67dfb385807d407f6bf73af8db2ddccdcd
David Milton <8712ce@gmail.com>
2024-08-04 19:32:28 -0500

    SOCKET.IO version: edited server js port listener and app dot get alternative route.  Also tweaked app.use express static.  Resolved 404 error, just have to figure out CORS error.

--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,24 @@ const http = require("http");
 const socketIo = require("socket.io");
 const cors = require("cors");
 const passport = require("./config.js/passport")();
-require("dotenv").config;
+require("dotenv").config();
 const methodOverride = require("method-override");
 const path = require("path");
-const router = express.Router();
+// const router = express.Router();
 
 
 
 // ACCESS MODELS //
-const db = require("./models")
+const db = require("./models");
 
 
 
 // ACCESS CONTROLLERS //
-const usersCtrl = require("./controllers/users.js")
-const studentsCtrl = require("./controllers/students.js")
-const teachersCtrl = require("./controllers/teachers.js")
-const vehiclesCtrl = require("./controllers/vehicles.js")
-
-const pathCoordinatesCtrl = require('./controllers/testPath.js')
+const usersCtrl = require("./controllers/users.js");
+const studentsCtrl = require("./controllers/students.js");
+const teachersCtrl = require("./controllers/teachers.js");
+const vehiclesCtrl = require("./controllers/vehicles.js");
+const pathCoordinatesCtrl = require('./controllers/testPath.js');
 
 
 
@@ -36,20 +35,22 @@ const io = socketIo(server);
 // MIDDLEWARE //
 
 // CORS ALLOWS THE FRONTEND TO COMMUNICATE WITH THE BACKEND //
-app.use(cors())
+app.use(cors());
 
 
 
 // BODY PARSER: USED FOR POST/PUT/PATCH ROUTES: THIS WILL TAKE INCOMING STRINGS FROM THE BODY THAT ARE URL ENCODED AND PARSE THEM INTO AN OBJECT THAT CAN BE ACCESSED IN TEH REQUEST PARAMETER AS A PROPERTY CALLED BODY. //
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.json())
-app.use(passport.initialize())
+app.use(express.json());
+app.use(passport.initialize());
 
 
 
 // SERVE STATIC ASSETS FROM THE 'ASSETS' DIRECTORY //
-app.use(express.static('assets'));
+// app.use(express.static('assets'));
+
+
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
 
 
@@ -67,15 +68,22 @@ app.use("/paths", pathCoordinatesCtrl);
 
 
 // ANY OTHER ROUTE NOT MATCHING THE ROUTES ABOVE GETS ROUTED BY REACT //
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(path.dirname(__dirname), "frontend", "build", "index.html"));
+// });
 app.get("*", (req, res) => {
-    res.sendFile(path.join(path.dirname(__dirname), "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
 
 
 // LISTENER //
-app.listen(process.env.PORT, () => {
-    console.log(`App is running at localhost:${process.env.PORT}`)
+// app.listen(process.env.PORT, () => {
+//     console.log(`App is running at localhost:${process.env.PORT}`)
+// });
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log(`App is running at localhost:${port}`);
 });
 
 
