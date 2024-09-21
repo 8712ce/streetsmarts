cc0e1095935c6a699eb21e1c8afe9626c72a8ba5
David Milton <8712ce@gmail.com>
2024-09-21 18:30:54 -0500

    simplified Automobile component to make it a presentational component that relies solely on props passed down from appjs

--- frontend/src/components/Automobile/index.js
+++ frontend/src/components/Automobile/index.js
@@ -1,41 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './automobile.css';
 
-const Automobile = ({ vehicle, onComplete, socket }) => {
-    const [automobilePosition, setAutomobilePosition] = useState(vehicle.currentPosition);
-
-    const [hasRemoved, setHasRemoved] = useState(false); // BOOLEAN FLAG FOR DEREGISTRATION //
-
-    useEffect(() => {
-        const handleUpdateVehicle = (updatedVehicle) => {
-            if (updatedVehicle._id === vehicle._id) {
-                console.log('Automobile component updating position:', updatedVehicle.currentPosition);
-                setAutomobilePosition(updatedVehicle.currentPosition);
-            }
-        };
-
-
-
-        const handleRemoveVehicle = (vehicleId) => {
-            console.log('Automobile component received removeVehicle for:', vehicleId);
-            if (vehicleId === vehicle._id && !hasRemoved) {
-                console.log('Automobile component removing vehicle:', vehicleId);
-                setHasRemoved(true); // PREVENT MULTIPLE REMOVALS //
-                onComplete(vehicleId); // CALL THE onComplete CALLBCK OT NOTIFY APP.JS //
-            }
-        };
-
-
-
-        socket.on('updateVehicle', handleUpdateVehicle);
-        socket.on('removeVehicle', handleRemoveVehicle);
-
-        return () => {
-            socket.off('updateVehicle', handleUpdateVehicle);
-            socket.off('removeVehicle', handleRemoveVehicle);
-        };
-    }, [vehicle, onComplete, socket, hasRemoved]);
-
+const Automobile = ({ vehicle }) => {
     return (
         <div className="automobile" style={{ transform: `translate(${automobilePosition.x}px, ${automobilePosition.y}px)` }}>
             {vehicle && <img src={vehicle.image} alt={vehicle.type} className="vehicle-image" />}
