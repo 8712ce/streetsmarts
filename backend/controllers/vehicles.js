34507bff30b5c3857d7e383e3550d91f3adaefd1
David Milton <8712ce@gmail.com>
2024-10-01 11:35:57 -0500

    updated vehicles.js by importing createVehicle from vehiclesService.

--- backend/controllers/vehicles.js
+++ backend/controllers/vehicles.js
@@ -27,7 +27,7 @@ router.post('/random', async (req, res) => {
         ]);
         console.log('Random vehicle template selected:', randomVehicleTemplate);
 
-        // IF THE PATH OF THE RANDOM VEHICLE TEMPLATE IS NULL, REPLACE IT WITH A RANDOM PATH //
+        // ASSIGN A RANDOM PATH IF NONE EXISTS //
         if (!randomVehicleTemplate[0].path || randomVehicleTemplate[0].path.length === 0) {
             const randomPath = await Path.aggregate([{ $sample: { size: 1 } }]);
             randomVehicleTemplate[0].path = randomPath[0].coordinates; // ASSUMING PATH IS STORED AS AN OBJECTID //
@@ -35,37 +35,25 @@ router.post('/random', async (req, res) => {
         }
 
         // CREATE A NEW VEHICLE OBJECT WITH A UNIQUE ID //
-        const newVehicle = new Vehicle({
+        const vehicleData = {
             type: randomVehicleTemplate[0].type,
             damage: randomVehicleTemplate[0].damage,
             image: randomVehicleTemplate[0].image,
             path: randomVehicleTemplate[0].path,
             currentPosition: randomVehicleTemplate[0].path[0] // SET INITIAL POSITION TO THE START OF THE PATH //
-        });
+        };
 
-        // SAVE THE NEW VEHICLE TO THE DATABASE //
-        const createdVehicle = await newVehicle.save();
-        console.log('New vehicle created and saved:', createdVehicle);
-        console.log('Vehicle _id type:', typeof createdVehicle._id);
-        console.log('Vehicle _id value:', createdVehicle._id);
+        // USE 'createVehicle' FUNCTION TO CREATE THE VEHICLE //
+        const createdVehicle = await createVehicle(vehicleData);
+        console.log('New vehicle created and started moving:', createdVehicle);
 
-        // EMIT THE NEW VEHICLE EVENT TO ALL CLIENTS //
-        const { io } = require('../server');
 
-        console.log('About to emit newVehicle event:', createdVehicle._id);
-
-        io.emit('newVehicle', createdVehicle);
-
-        console.log('newVehicle event emitted for vehicle ID:', createdVehicle._id);
-
-        // START MOVING THE VEHICLE //
-        moveVehicle(createdVehicle._id);
 
         // RETURN THE CREATED VEHICLE AS JSON RESPONSE //
         res.json(createdVehicle);
     } catch (err) {
         console.error('Error creating a new vehicle:', err);
-        res.status(500).json({ message: 'Server Error' });
+        res.status(500).json({ message: 'Server Error', error: err.message });
     }
 });
 
