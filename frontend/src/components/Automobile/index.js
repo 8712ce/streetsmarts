7da3ca4098e006af4e57cd784f62bc2b65bd6fb5
David Milton <8712ce@gmail.com>
2024-11-03 19:23:26 -0600

    updated Automobile component to use scaled positioning.

--- frontend/src/components/Automobile/index.js
+++ frontend/src/components/Automobile/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './automobile.css';
 
-const Automobile = ({ vehicle }) => {
+// FUNCTION TO CALCULATE THE SCALED POSITION BASED ON CONTAINER DIMENSIONS //
+const calculateScaledPosition = (positionX, positionY, containerWidth, containerHeight) => {
+    return {
+        x: (positionX / 100) * containerWidth,
+        y: (positionY / 100) * containerHeight,
+    };
+};
+
+const Automobile = ({ vehicle, containerWidth, containerHeight }) => {
+    const { x, y } = calculateScaledPosition(vehicle.currentPosition.x, vehicle.currentPosition.y, containerWidth, containerHeight);
+
     return (
-        <div className="automobile" style={{ transform: `translate(${vehicle.currentPosition.x}px, ${vehicle.currentPosition.y}px)` }}>
+        <div className="automobile" style={{ position: 'absolute', transform: `translate(${x}px, ${y}px)` }}>
             {vehicle && <img src={vehicle.image} alt={vehicle.type} className="vehicle-image" />}
         </div>
     );
