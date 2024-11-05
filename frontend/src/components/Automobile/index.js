2202cb5959fe3ec09b227135e03003a01cb271ff
David Milton <8712ce@gmail.com>
2024-11-05 13:26:55 -0600

    updated automobile component to position vehicles relative to the background image.

--- frontend/src/components/Automobile/index.js
+++ frontend/src/components/Automobile/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './automobile.css';
 
 const Automobile = ({ vehicle }) => {
+    const containerWidth = 3000; // WIDTH OF BACKGROUND IMAGE //
+    const containerHeight = 1000; // HEIGHT OF BACKGROUND IMAGE //
+
     return (
         <div
             className="automobile"
             style={{
-                position: 'absolute',
-                left: `${vehicle.currentPosition.x}%`,
-                top: `${vehicle.currentPosition.y}%`,
+                // position: 'absolute',
+                left: `${(vehicle.currentPosition.x / 100) * containerWidth}px`,
+                top: `${(vehicle.currentPosition.y / 100) * containerHeight}px`,
                 transform: 'translate(-50%, -100%)',
             }}
         >
