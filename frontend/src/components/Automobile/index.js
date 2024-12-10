c26dc0fcaf4799a84dd6bcf5acde80c9d6f8c6af
David Milton <8712ce@gmail.com>
2024-12-10 13:08:34 -0600

    added a modal-root div to public slash index dot html.

--- frontend/src/components/Automobile/index.js
+++ frontend/src/components/Automobile/index.js
@@ -5,16 +5,6 @@ import { calculateZIndex } from '../../utils/zIndexUtils';
 const Automobile = ({ vehicle }) => {
     const zIndex = calculateZIndex(vehicle.currentPosition.y, vehicle._id);
 
-    // const maxZIndex = 1000;
-    // const maxY = 100;
-
-    // // CALCULATE Z-INDEX BASED ON Y POSITION //
-    // let zIndex = Math.floor((vehicle.currentPosition.y / maxY) * maxZIndex);
-
-    // // HANDLE EDGE CASES: ADD A SMALL OFFSET BASED ON VEHICLE ID //
-    // const idOffset = parseInt(vehicle._id, 36) % maxZIndex;
-    // zIndex = zIndex * maxZIndex + idOffset;
-
     return (
         <div
             className="automobile"
