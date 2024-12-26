76cef11f5428dd9de416ac98005f39a20ede774f
David Milton <8712ce@gmail.com>
2024-12-25 18:37:34 -0600

    Updated app dot js by importing DifficultyMenu and SimulationContainer and routeing to them.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
 // DEPENDENCIES //
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 
 // PAGES //
 import Login from "./pages/login/login";
 import SignUp from "./pages/signUp/signUp";
 
 // COMPONENTS //
-import Home from "./components/Home";
-import FourWayStopSigns from "./components/FourWayStopSigns";
-import FourWayTrafficSignals from "./components/FourWayTrafficSignals";
+// import Home from "./components/Home";
+// import FourWayStopSigns from "./components/FourWayStopSigns";
+// import FourWayTrafficSignals from "./components/FourWayTrafficSignals";
+import DifficultyMenu from "./components/DifficultyMenu";
+import SimulationContainer from "./components/SimulationContainer";
 
 // STYLES //
 import "./App.css";
 
 function App() {
   return (
+      // <Routes>
+      //   <Route path="/" element={<Home />} />
+      //   <Route path="/four_way_stop_signs" element={<FourWayStopSigns />} />
+      //   <Route path="/four_way_traffic_signals" element={<FourWayTrafficSignals />} />
+      // </Routes>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/four_way_stop_signs" element={<FourWayStopSigns />} />
-        <Route path="/four_way_traffic_signals" element={<FourWayTrafficSignals />} />
-      </Routes>
+        <Route path="/" element={<DifficultyMenu />} />
+        <Route path="/simulation" element={SimulationContainer />} />
+      </Routes>>
   )
 }
 
