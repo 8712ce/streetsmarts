7911bde972207a44715f206cff196c6ba8c6632c
David Milton <8712ce@gmail.com>
2025-01-02 21:03:27 -0600

    Added SignUp route to App dot js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 
-// PAGES //
-import Login from "./pages/login/login";
-import SignUp from "./pages/signUp/signUp";
-import SignIn from "./components/SignIn";
 
 // COMPONENTS //
 // import Home from "./components/Home";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 import SelectionMenu from "./components/SelectionMenu";
 import FourWayStopSigns from "./components/FourWayStopSigns";
 import FourWayTrafficSignals from "./components/FourWayTrafficSignals";
-import DifficultyMenu from "./components/DifficultyMenu";
-import SimulationContainer from "./components/SimulationContainer";
+// import DifficultyMenu from "./components/DifficultyMenu";
+// import SimulationContainer from "./components/SimulationContainer";
 
 // STYLES //
 import "./App.css";
@@ -23,6 +21,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/menu" element={<SelectionMenu />} />
         <Route path="/four_way_stop_signs" element={<FourWayStopSigns />} />
         <Route path="/four_way_traffic_signals" element={<FourWayTrafficSignals />} />
