efb6a13c656cc6d4a444bbdfbfc309424c9f0ab8
David Milton <8712ce@gmail.com>
2025-01-07 09:31:05 -0600

    Updated user model to include role.

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
-})
+    password: {type: String, required: true},
+    role: { type: String, enum: ['teacher', 'student'], required: true, default: 'student'}
+});
 
 const User = mongoose.model('User', userSchema)
 
