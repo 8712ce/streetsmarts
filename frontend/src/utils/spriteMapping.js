405e8ec29238910b8c6902a773f800a8f49a2c3a
David Milton <8712ce@gmail.com>
2025-01-23 20:03:21 -0600

    Created a spriteMapping util.

---
+++ frontend/src/utils/spriteMapping.js
@@ -0,0 +1,173 @@
+const spriteMapping = {
+    N_R: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/N_R_1.png',
+        },
+        {
+            range: [3, 4, 5], // INTERSECTION //
+            sprite: '/assets/N_R_2.png',
+        },
+        {
+            range: [6, 7, 8, 9], // FINAL PORTION //
+            sprite: '/assets/N_R_3.png',
+        },
+    ],
+    N_S: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/N_R_1.png',
+        },
+        {
+            range: [3, 4, 5], // INTERSECTION //
+            sprite: '/assets/N_S_2.png',
+        },
+        {
+            range: [6, 7, 8], // FINAL PORTION //
+            sprite: '/assets/N_S_3.png',
+        },
+    ],
+    N_L: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/N_R_1.png',
+        },
+        {
+            range: [3, 4, 5, 6], // INTERSECTION //
+            sprite: '/assets/N_L_2.png',
+        },
+        {
+            range: [7, 8, 9, 10], // FINAL PORTION //
+            sprite: '/assets/N_L_3.png',
+        },
+    ],
+
+    E_R: [
+        {
+            range: [0, 1, 2, 3], // INDICES 0..2 //
+            sprite: '/assets/E_R_1.png',
+        },
+        {
+            range: [4, 5, 6], // INTERSECTION //
+            sprite: '/assets/E_R_2.png',
+        },
+        {
+            range: [7, 8, 9], // FINAL PORTION //
+            sprite: '/assets/E_R_3.png',
+        },
+    ],
+    E_S: [
+        {
+            range: [0, 1, 2, 3], // INDICES 0..2 //
+            sprite: '/assets/E_S_1.png',
+        },
+        {
+            range: [3, 4, 5], // INTERSECTION //
+            sprite: '/assets/E_S_2.png',
+        },
+        {
+            range: [6, 7, 8], // FINAL PORTION //
+            sprite: '/assets/E_S_3.png',
+        },
+    ],
+    E_L: [
+        {
+            range: [0, 1, 2, 3], // INDICES 0..2 //
+            sprite: '/assets/E_L_1.png',
+        },
+        {
+            range: [3, 4, 5, 6], // INTERSECTION //
+            sprite: '/assets/E_L_2.png',
+        },
+        {
+            range: [7, 8, 9, 10], // FINAL PORTION //
+            sprite: '/assets/E_L_3.png',
+        },
+    ],
+    S_R: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/S_R_1.png',
+        },
+        {
+            range: [3, 4, 5], // INTERSECTION //
+            sprite: '/assets/S_R_2.png',
+        },
+        {
+            range: [6, 7, 8, 9], // FINAL PORTION //
+            sprite: '/assets/S_R_3.png',
+        },
+    ],
+    S_S: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/S_S_1.png',
+        },
+        {
+            range: [3, 4, 5], // INTERSECTION //
+            sprite: '/assets/S_S_2.png',
+        },
+        {
+            range: [6, 7, 8], // FINAL PORTION //
+            sprite: '/assets/S_S_3.png',
+        },
+    ],
+    S_L: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/S_L_1.png',
+        },
+        {
+            range: [3, 4, 5, 6], // INTERSECTION //
+            sprite: '/assets/S_L_2.png',
+        },
+        {
+            range: [7, 8, 9, 10], // FINAL PORTION //
+            sprite: '/assets/S_L_3.png',
+        },
+    ],
+    W_R: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/W_R_1.png',
+        },
+        {
+            range: [3, 4, 5], // INTERSECTION //
+            sprite: '/assets/W_R_2.png',
+        },
+        {
+            range: [6, 7, 8, 9], // FINAL PORTION //
+            sprite: '/assets/W_R_3.png',
+        },
+    ],
+    W_S: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/W_S_1.png',
+        },
+        {
+            range: [3, 4, 5], // INTERSECTION //
+            sprite: '/assets/W_S_2.png',
+        },
+        {
+            range: [6, 7, 8], // FINAL PORTION //
+            sprite: '/assets/W_S_3.png',
+        },
+    ],
+    W_L: [
+        {
+            range: [0, 1, 2], // INDICES 0..2 //
+            sprite: '/assets/W_L_1.png',
+        },
+        {
+            range: [3, 4, 5, 6], // INTERSECTION //
+            sprite: '/assets/W_L_2.png',
+        },
+        {
+            range: [7, 8, 9, 10], // FINAL PORTION //
+            sprite: '/assets/W_L_3.png',
+        },
+    ],
+}
+
+export default spriteMapping;
