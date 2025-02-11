e42681f1bb95b4c23697eefce93030efe706f72b
David Milton <8712ce@gmail.com>
2025-02-11 13:49:49 -0600

    Removed some commented out code in EyeContact.

--- frontend/src/components/EyeContact/eyeContact.js
+++ frontend/src/components/EyeContact/eyeContact.js
@@ -2,22 +2,16 @@ import React, { useEffect, useState } from "react";
 import { getVehicleSprite, getBackgroundPosition } from "../../utils/spriteUtils";
 import './eyeContact.css';
 
-const EyeContact = ({ vehicle, onClose }) => {
+const EyeContact = ({ vehicle }) => {
     const [spriteStyle, setSpriteStyle] = useState({});
 
     useEffect(() => {
-        // DETERMINE THE CURRENT FRAME INDEX //
-        // const frameIndex = vehicle.displayImage ? vehicle.displayImage.frameIndex : 0;
-
         // COMPUTE SPRITE DATA IF NOT ALREADY PROVIDED //
         const spriteData = vehicle.displayImage || getVehicleSprite(vehicle);
         const { frameIndex } = spriteData;
 
         // USE YOUR SPRITE UTILITY TO GET THE BACKGROUND POSITION //
         const { backgroundPositionX, backgroundPositionY } = getBackgroundPosition(frameIndex);
-
-        // // DEFINE A ZOOM FACTOR TO "CLOSE UP" THE SPRITE //
-        // const zoomFactor = 3;
 
         setSpriteStyle({
             backgroundImage: `url(${vehicle.image})`,
